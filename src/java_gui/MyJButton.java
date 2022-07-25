8713628ebfddc9ca02eaad4d017e0635b7271edc
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-07-25 13:15:54 +0300

    "JButton done"

--- src/java_gui/MyJButton.java
+++ src/java_gui/MyJButton.java
@@ -8,24 +8,46 @@ import java.awt.event.ActionListener;
 public class MyJButton {
     //JButton = a button that performs an action when clicked...
     private JButton button = new JButton();
+    private JLabel label;
+
 
     public MyJButton() {
-        this.button.setText("I'm a button!");
-        this.button.setBounds(200,200,100,50);
-        this.button.setFocusable(false); // when false removes "glow" around button
-        this.button.addActionListener(e -> System.out.println("Foo"));
+        ImageIcon buttonImg = new ImageIcon("/Users/tanyashkolnik/IdeaProjects/SwingJava/src/classical_apple.png");
+        ImageIcon labelImg = new ImageIcon("/Users/tanyashkolnik/IdeaProjects/SwingJava/src/psyduck.jpeg");
 
+        label = new JLabel();
+        label.setIcon(labelImg);
+        label.setBounds(200,150,150,150);
+        label.setVisible(false);
+
+        this.button.setText("I'm a button!");
+        this.button.setBounds(150,100,200,160);
+        this.button.setFocusable(false); // when false removes "glow" around button
+        this.button.setIcon(buttonImg);
+        this.button.setHorizontalTextPosition(JButton.CENTER);
+        this.button.setVerticalTextPosition(JButton.BOTTOM);
+        //this.button.setBorder(BorderFactory.createEtchedBorder());
+
+
+        //this.button.addActionListener(e -> System.out.println("Foo"));
+
+        this.button.addActionListener(e -> {
+                System.out.println("Foo");
+                //button.setEnabled(false); //disable button after click!
+            label.setVisible(true);
+        });
     }
+
 
 
 
     public static void main(String[] args){
 
         MyFrame frame = new MyFrame(500,500,"JButton lesson");
+        MyJButton but = new MyJButton();
+        frame.getContentPane().add(but.button);
+        frame.getContentPane().add(but.label);
 
-        frame.getContentPane().add(new MyJButton().button);
-
-        //frame.setVisible(true);
 
     }
 }
